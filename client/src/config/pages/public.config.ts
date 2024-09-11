776d740afa1238619f85e786ae21b4accb0582b1
nfdmitry <khv1702@gmail.com>
2024-09-12 02:41:49 +0300

    Added a link to a page with information about prices for premium content

--- client/src/config/pages/public.config.ts
+++ client/src/config/pages/public.config.ts
@@ -2,7 +2,8 @@ class PublicPages {
 	HOME = '/'
 	LOGIN = '/login'
 	REGISTER = '/register'
-	CONTENT = '/content'
+	FREE_CONTENT = '/free-content'
+	PREMIUM_CONTENT_PRICE = '/premium-content-price'
 }
 
 export const PUBLIC_PAGES = new PublicPages()
