f4d69f355cc65ff3d6bea5d05a3a2d40d3e24449
nfdmitry <khv1702@gmail.com>
2024-09-12 02:47:15 +0300

    Added config

---
+++ client/src/utils/transform-user-to-state.ts
@@ -0,0 +1,25 @@
+import {
+	UserRole,
+	type TProtectUserData
+} from '@/services/auth/auth.types'
+
+export type TUserDataState = {
+	id: number
+	rights: UserRole[]
+	isLoggedIn: boolean
+	isAdmin: boolean
+	isManager: boolean
+	isPremium: boolean
+}
+
+export const transformUserToState = (
+	user: TProtectUserData
+): TUserDataState | null => {
+	return {
+		...user,
+		isLoggedIn: true,
+		isAdmin: user.rights.includes(UserRole.ADMIN),
+		isManager: user.rights.includes(UserRole.MANAGER),
+		isPremium: user.rights.includes(UserRole.PREMIUM)
+	}
+}
