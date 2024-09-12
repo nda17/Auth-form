1b54bce6d7c524cbf63963771c5c5f7467ad70d4
nfdmitry <khv1702@gmail.com>
2024-09-12 13:54:59 +0300

    Added ui component Heading

--- client/src/components/screens/premium-content/PremiumContent.tsx
+++ client/src/components/screens/premium-content/PremiumContent.tsx
@@ -1,17 +1,20 @@
 'use client'
+import styles from '@/components/screens/premium-content/PremiumContent.module.scss'
 import CirclesLoader from '@/components/ui/circles-loader/CirclesLoader'
+import Heading from '@/components/ui/heading/Heading'
 import UserService from '@/services/user.service'
 import { useQuery } from '@tanstack/react-query'
+import { FC } from 'react'
 
-const PremiumContent = () => {
+const PremiumContent: FC = () => {
 	const { data, isLoading } = useQuery({
 		queryKey: ['premium-content'],
 		queryFn: () => UserService.fetchPremium()
 	})
 
 	return (
-		<div>
-			<h1>Only for premium users</h1>
+		<div className={styles.wrapper}>
+			<Heading text="Page for users with a purchased Premium subscription" />
 			{isLoading ? (
 				<CirclesLoader />
 			) : (
