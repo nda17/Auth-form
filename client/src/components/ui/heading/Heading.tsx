b8e4ab0c8410d73b63be79676fbb8c888d692994
nfdmitry <khv1702@gmail.com>
2024-09-12 13:03:44 +0300

    Added component

---
+++ client/src/components/ui/heading/Heading.tsx
@@ -0,0 +1,9 @@
+import styles from '@/components/ui/heading/Heading.module.scss'
+import { IHeading } from '@/components/ui/heading/heading.interface'
+import { FC } from 'react'
+
+const Heading: FC<IHeading> = ({ text }) => {
+	return <h1 className={styles.heading}>{text}</h1>
+}
+
+export default Heading
