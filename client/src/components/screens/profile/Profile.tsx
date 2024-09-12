7e1f29d49f6e0836c684f3cc4b112d29f1227549
nfdmitry <khv1702@gmail.com>
2024-09-12 16:05:07 +0300

    Refactor

--- client/src/components/screens/profile/Profile.tsx
+++ client/src/components/screens/profile/Profile.tsx
@@ -2,6 +2,7 @@
 import { useProfile } from '@/app/hooks/useProfile'
 import styles from '@/components/screens/profile/Profile.module.scss'
 import CirclesLoader from '@/components/ui/circles-loader/CirclesLoader'
+import Heading from '@/components/ui/heading/Heading'
 import { PUBLIC_PAGES } from '@/config/pages/public.config'
 import authService from '@/services/auth/auth.service'
 import { useMutation } from '@tanstack/react-query'
@@ -31,50 +32,59 @@ const Profile: FC = () => {
 
 	const isLogoutLoading = isLogoutPending || isPending
 
-	if (isLoading) {
-		return <CirclesLoader />
-	}
-
 	return (
 		<div className={styles.wrapper}>
-			{user.avatarPath ? (
-				<Image
-					className={styles.image}
-					src={user.avatarPath}
-					alt="Avatar"
-					width={70}
-					height={70}
-				/>
+			<Heading text="Profile" />
+
+			{isLoading ? (
+				<CirclesLoader />
 			) : (
-				<Image
-					className={styles.image}
-					src={'/avatar-default.png'}
-					alt="Avatar"
-					width={70}
-					height={70}
-				/>
+				<>
+					<div className={clsx(styles['user-info'])}>
+						{user.avatarPath ? (
+							<Image
+								className={styles.image}
+								src={user.avatarPath}
+								alt="Avatar"
+								width={70}
+								height={70}
+							/>
+						) : (
+							<Image
+								className={styles.image}
+								src={'/avatar-default.png'}
+								alt="Avatar"
+								width={70}
+								height={70}
+							/>
+						)}
+						<h2 className={styles.subtitle}>
+							Hello, {user.name || 'user'}
+						</h2>
+					</div>
+					<div></div>
+					<p className={clsx(styles['info-field'])}>
+						Email: {user.email}{' '}
+						<i>
+							(
+							{user.verificationToken
+								? 'Confirmation is required'
+								: 'Confirmed'}
+							)
+						</i>
+					</p>
+					<p className={clsx(styles['info-field'])}>
+						Role: {user.rights?.join(', ')}
+					</p>
+					<button
+						onClick={() => mutateLogout()}
+						disabled={isLogoutLoading}
+						className={clsx(styles['logout-button'])}
+					>
+						{isLogoutLoading ? 'Wait...' : 'Logout'}
+					</button>
+				</>
 			)}
-			<h2 className={styles.subtitle}>Hello, {user.name || 'user'}</h2>
-			<p className={clsx(styles['info-field'])}>
-				Email: {user.email}{' '}
-				<i>
-					(
-					{user.verificationToken
-						? 'Confirmation is required'
-						: 'Confirmed'}
-					)
-				</i>
-			</p>
-			<p className={clsx(styles['info-field'])}>
-				Role: {user.rights?.join(', ')}
-			</p>
-			<button
-				onClick={() => mutateLogout()}
-				disabled={isLogoutLoading}
-				className={clsx(styles['logout-button'])}
-			>
-				{isLogoutLoading ? 'Wait...' : 'Logout'}
-			</button>
 		</div>
 	)
 }
