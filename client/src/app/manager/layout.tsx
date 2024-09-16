26651bd8743c4389ba7031064c6cb23b6e13c5d6
nfdmitry <khv1702@gmail.com>
2024-09-17 01:02:49 +0300

    Delete files

--- client/src/app/manager/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { UserRole } from '@/services/auth/auth.types'
-import { protectPage } from '@/utils/server/protect-page'
-import type { PropsWithChildren } from 'react'
-
-export const ManagerLayout = async ({ children }: PropsWithChildren) => {
-	await protectPage(UserRole.MANAGER)
-
-	return children
-}
