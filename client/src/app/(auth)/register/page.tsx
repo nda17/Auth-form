f02a32a372b1baa4b2d5a13c6e26fb94804bde47
nfdmitry <khv1702@gmail.com>
2024-09-16 04:27:10 +0300

    Added metadata

--- client/src/app/(auth)/register/page.tsx
+++ client/src/app/(auth)/register/page.tsx
@@ -2,7 +2,8 @@ import SignUp from '@/components/screens/(auth)/register/SignUp'
 import type { Metadata, NextPage } from 'next'
 
 export const metadata: Metadata = {
-	title: 'Sign up'
+	title: 'Sign up',
+	description: 'Sign up page'
 }
 
 const RegisterPage: NextPage = () => {
