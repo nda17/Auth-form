6bb83e9e84623e64ca1cf63c66e182160e47d03e
nfdmitry <khv1702@gmail.com>
2024-09-18 09:41:13 +0300

    Added service

---
+++ client/src/services/auth/auth.service.ts
@@ -0,0 +1,74 @@
+import { axiosClassicRequest } from '@/api/interceptors'
+import { IFormData } from '@/shared/types/form.types'
+import { IUser } from '@/shared/types/user.types'
+import { removeFromStorage, saveTokenStorage } from './auth.helper'
+
+interface IAuthResponse {
+	accessToken: string
+	user: IUser
+}
+
+export enum EnumTokens {
+	'ACCESS_TOKEN' = 'accessToken',
+	'REFRESH_TOKEN' = 'refreshToken'
+}
+
+class AuthService {
+	async main(
+		type: 'login' | 'register',
+		data: IFormData,
+		token?: string | null
+	) {
+		const response = await axiosClassicRequest.post<IAuthResponse>(
+			`/auth/${type}`,
+			data,
+			{
+				headers: {
+					recaptcha: token
+				}
+			}
+		)
+
+		if (response.data.accessToken)
+			saveTokenStorage(response.data.accessToken)
+
+		return response
+	}
+
+	async getNewTokens() {
+		const response = await axiosClassicRequest.post<IAuthResponse>(
+			'/auth/access-token'
+		)
+
+		if (response.data.accessToken)
+			saveTokenStorage(response.data.accessToken)
+
+		return response
+	}
+
+	async getNewTokensByRefresh(refreshToken: string) {
+		const response = await axiosClassicRequest.post<IAuthResponse>(
+			'/auth/access-token',
+			{},
+			{
+				headers: {
+					Cookie: `refreshToken=${refreshToken}`
+				}
+			}
+		)
+
+		return response.data
+	}
+
+	async logout() {
+		const response = await axiosClassicRequest.post<boolean>(
+			'/auth/logout'
+		)
+
+		if (response.data) removeFromStorage()
+
+		return response
+	}
+}
+
+export default new AuthService()
