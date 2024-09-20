4c5354a0bff7cb7967877603ca0df83a8d36616c
nfdmitry <khv1702@gmail.com>
2024-09-21 01:04:52 +0300

    Added manager page middleware

---
+++ client/src/app/middlewares/managerMiddleware.ts
@@ -0,0 +1,19 @@
+import { UserRole } from '@/services/auth/auth.types'
+import { getServerAuth } from '@/utils/server/get-server-auth'
+import { NextRequest, NextResponse } from 'next/server'
+
+export const managerMiddleware = async (request: NextRequest) => {
+	const user = await getServerAuth()
+
+	if (
+		user.isLoggedIn &&
+		user.isManager &&
+		user.rights?.includes(UserRole.MANAGER)
+	) {
+		NextResponse.redirect(new URL('/manager', request.url))
+	} else {
+		return NextResponse.error()
+	}
+
+	return NextResponse.next()
+}
