1cc72a95c41c6cf5938b279e3ef845e5009c7f21
nfdmitry <khv1702@gmail.com>
2024-09-21 01:04:19 +0300

    Refactor, fix bugs

--- client/src/app/middlewares/profileMiddleware.ts
+++ client/src/app/middlewares/profileMiddleware.ts
@@ -1,23 +1,13 @@
-import { EnumTokens } from '@/services/auth/auth.service'
+import { UserRole } from '@/services/auth/auth.types'
 import { getServerAuth } from '@/utils/server/get-server-auth'
 import { NextRequest, NextResponse } from 'next/server'
 
 export const profileMiddleware = async (request: NextRequest) => {
-	const refreshToken = request.cookies.get(EnumTokens.REFRESH_TOKEN)?.value
-	const accessToken = request.cookies.get(EnumTokens.ACCESS_TOKEN)?.value
+	const user = await getServerAuth()
 
-	if (!refreshToken) {
-		const response = NextResponse.redirect(new URL('/login', request.url))
-		response.cookies.set(EnumTokens.ACCESS_TOKEN, '', {
-			expires: new Date(0),
-			path: '/',
-			domain: 'localhost'
-		})
-
-		return response
-	} else if (!accessToken) {
-		await getServerAuth()
-	}
+	if (!user.isLoggedIn) {
+		return NextResponse.redirect(new URL('/login', request.url))
+	} 
 
 	return NextResponse.next()
 }
