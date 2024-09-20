06029f16066722d5134d51887ef95560d389841f
nfdmitry <khv1702@gmail.com>
2024-09-21 01:31:16 +0300

    Added SubHeading component

--- client/src/components/screens/free-content/FreeContent.tsx
+++ client/src/components/screens/free-content/FreeContent.tsx
@@ -1,8 +1,15 @@
+import styles from '@/components/screens/free-content/FreeContent.module.scss'
 import Heading from '@/components/ui/heading/Heading'
+import SubHeading from '@/components/ui/sub-heading/SubHeading'
 import { FC } from 'react'
 
 const FreeContent: FC = () => {
-	return <Heading text="Free content page for all users" />
+	return (
+		<div className={styles.wrapper}>
+			<Heading text="Free content page for all users" />
+			<SubHeading text="Free content" />
+		</div>
+	)
 }
 
 export default FreeContent
