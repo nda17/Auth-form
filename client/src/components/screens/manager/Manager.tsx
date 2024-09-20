015f5303eb14cb9bf6a7c81263a4c6d8a3e4b25b
nfdmitry <khv1702@gmail.com>
2024-09-21 01:31:43 +0300

    Refactor

--- client/src/components/screens/manager/Manager.tsx
+++ client/src/components/screens/manager/Manager.tsx
@@ -1,20 +1,25 @@
 'use client'
+import styles from '@/components/screens/manager/Manager.module.scss'
+import CirclesLoader from '@/components/ui/circles-loader/CirclesLoader'
+import Heading from '@/components/ui/heading/Heading'
+import SubHeading from '@/components/ui/sub-heading/SubHeading'
 import UserService from '@/services/user.service'
 import { useQuery } from '@tanstack/react-query'
+import { FC } from 'react'
 
-const Manager = () => {
+const Manager: FC = () => {
 	const { data, isLoading } = useQuery({
 		queryKey: ['manager-content'],
 		queryFn: () => UserService.fetchManager()
 	})
 
 	return (
-		<div>
-			<h1>Only for Managers:</h1>
+		<div className={styles.wrapper}>
+			<Heading text="Page for only managers" />
 			{isLoading ? (
-				<div>Loading...</div>
+				<CirclesLoader />
 			) : (
-				<p>{data?.data.text || 'Not found!'}</p>
+				<SubHeading text={`${data?.data.text || 'Not found!'}`} />
 			)}
 		</div>
 	)
