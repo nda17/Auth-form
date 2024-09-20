039a3f96a6452b56b0d0855a3f9f7b7270c1b48f
nfdmitry <khv1702@gmail.com>
2024-09-21 01:04:34 +0300

    Refactor, fix bugs

--- client/src/app/middlewares/adminMiddleware.ts
+++ client/src/app/middlewares/adminMiddleware.ts
@@ -1,34 +1,18 @@
-import { EnumTokens } from '@/services/auth/auth.service'
 import { UserRole } from '@/services/auth/auth.types'
 import { getServerAuth } from '@/utils/server/get-server-auth'
 import { NextRequest, NextResponse } from 'next/server'
 
 export const adminMiddleware = async (request: NextRequest) => {
-	const refreshToken = request.cookies.get(EnumTokens.REFRESH_TOKEN)?.value
+	const user = await getServerAuth()
 
-	if (!refreshToken) {
-		const response = NextResponse.redirect(
-			new URL('/not-found', request.url)
-		)
-		response.cookies.set(EnumTokens.ACCESS_TOKEN, '', {
-			expires: new Date(0),
-			path: '/',
-			domain: 'localhost'
-		})
-
-		return response
-	}
-
-	if (request.nextUrl.pathname.includes('/admin')) {
-		const user = await getServerAuth()
-
-		if (!user) {
-			return NextResponse.error()
-		} else if (user?.rights?.includes(UserRole.ADMIN)) {
-			NextResponse.redirect(new URL('/admin', request.url))
-		} else {
-			return NextResponse.error()
-		}
+	if (
+		user.isLoggedIn &&
+		user.isAdmin &&
+		user.rights?.includes(UserRole.ADMIN)
+	) {
+		NextResponse.redirect(new URL('/admin', request.url))
+	} else {
+		return NextResponse.error()
 	}
 
 	return NextResponse.next()
