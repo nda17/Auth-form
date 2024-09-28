7b8722b6983d9d65f2b87caed6d3a1e8214eea19
nfdmitry <khv1702@gmail.com>
2024-09-28 03:43:27 +0300

    Deleted files

--- client/src/components/screens/home/home.interface.ts
+++
@@ -1,4 +0,0 @@
-import { TUserDataState } from '@/utils/transform-user-to-state'
-
-export interface IHome
-	extends Pick<TUserDataState, 'isLoggedIn' | 'isAdmin' | 'isManager'> {}
--- client/src/utils/store/changeStatusAuth.ts
+++
@@ -1,8 +0,0 @@
-import { setAuthStatus } from '@/store/auth-status/auth-status.slice'
-import { useDispatch } from 'react-redux'
-
-export const changeStatusAuth = async (status: boolean) => {
-	const dispatch = useDispatch()
-
-	dispatch(setAuthStatus(status))
-}
