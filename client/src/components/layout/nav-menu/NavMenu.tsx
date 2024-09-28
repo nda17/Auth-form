15d0ed40fd3a67dad4b8b937a657d5493f77c192
nfdmitry <khv1702@gmail.com>
2024-09-28 04:08:55 +0300

    Added navigation menu component

---
+++ client/src/components/layout/nav-menu/NavMenu.tsx
@@ -0,0 +1,44 @@
+import styles from '@/components/layout/nav-menu/NavMenu.module.scss'
+import DesktopStaticMenu from '@/components/layout/nav-menu/desktop/desktop-static-menu/DesktopStaticMenu'
+import LogoutButton from '@/components/layout/nav-menu/logout-button/LogoutButton'
+import MaterialIcon from '@/components/ui/icons/MaterialIcon'
+import useAuth from '@/hooks/useAuth'
+import { setAuthStatus } from '@/store/auth-status/auth-status.slice'
+import clsx from 'clsx'
+import { NextPage } from 'next'
+import Link from 'next/link'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+const NavMenu: NextPage = () => {
+	const statusAuth = useAuth()
+
+	const isLoggedIn = useSelector(
+		(state: any) => state.authStatus.isLoggedIn
+	)
+
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		dispatch(setAuthStatus(statusAuth ? true : false))
+	}, [statusAuth])
+
+	return (
+		<div className={styles.wrapper}>
+			<DesktopStaticMenu />
+
+			<div className={styles.wrapper}>
+				{!isLoggedIn ? (
+					<Link href="/login" className={clsx(styles['link-auth-button'])}>
+						<MaterialIcon name="MdLogout" fill="green" />
+						Login
+					</Link>
+				) : (
+					<LogoutButton />
+				)}
+			</div>
+		</div>
+	)
+}
+
+export default NavMenu
