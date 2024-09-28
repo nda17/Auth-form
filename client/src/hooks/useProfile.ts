d3abb7f5036e54d3a0523cad72c5d63e3736a534
nfdmitry <khv1702@gmail.com>
2024-09-28 04:45:40 +0300

    Added custom hook

---
+++ client/src/hooks/useProfile.ts
@@ -0,0 +1,45 @@
+import { saveTokenStorage } from '@/services/auth/auth.helper'
+import authService from '@/services/auth/auth.service'
+import userService from '@/services/user.service'
+import { transformUserToState } from '@/utils/transform-user-to-state'
+import { useQuery } from '@tanstack/react-query'
+import { useEffect } from 'react'
+
+const useProfile = () => {
+	const { data, isLoading } = useQuery({
+		queryKey: ['get-profile'],
+		queryFn: () => userService.fetchProfile(),
+		refetchInterval: 1800000 // 30 minutes in milliseconds
+	})
+
+	const { isSuccess, data: dataTokens } = useQuery({
+		queryKey: ['new-tokens'],
+		queryFn: () => authService.getNewTokens(),
+		enabled: !data?.data
+	})
+
+	useEffect(() => {
+		if (!isSuccess) {
+			return
+		}
+
+		if (dataTokens.data.accessToken) {
+			saveTokenStorage(dataTokens.data.accessToken)
+		}
+	}, [isSuccess])
+
+	const profile = data?.data
+
+	const userState = profile ? transformUserToState(profile) : null
+
+	return {
+		isLoading,
+		isSuccess,
+		user: {
+			...profile,
+			...userState
+		}
+	}
+}
+
+export default useProfile
