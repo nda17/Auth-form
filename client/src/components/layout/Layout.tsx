d4325df010856585afe2239b7d015798406d7586
nfdmitry <khv1702@gmail.com>
2024-09-28 04:05:28 +0300

    Refactor

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -2,9 +2,9 @@ import styles from '@/components/layout/Layout.module.scss'
 import Footer from '@/components/layout/footer/Footer'
 import Header from '@/components/layout/header/Header'
 import { ILayout } from '@/components/layout/layout.interface'
-import { FC } from 'react'
+import { NextPage } from 'next'
 
-const Layout: FC<ILayout> = ({ children }) => {
+const Layout: NextPage<ILayout> = ({ children }) => {
 	return (
 		<div className={styles.layout}>
 			<Header />
