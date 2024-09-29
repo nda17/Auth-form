5019a74d9dba2d1a3f4b611a9640e6790327c397
nfdmitry <khv1702@gmail.com>
2024-09-30 01:54:50 +0300

    Added navigation provider component

---
+++ client/src/providers/navigation-provider/NavigationProvider.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { usePathname, useSearchParams } from 'next/navigation'
+import { createContext, useContext, useEffect, useState } from 'react'
+
+export const useNavigationContext = () => useContext(NavigationContext)
+
+export const NavigationProvider = ({
+	children
+}: {
+	children: React.ReactNode
+}) => {
+	const navigation = useNavigation()
+
+	return (
+		<NavigationContext.Provider value={navigation}>
+			{children}
+		</NavigationContext.Provider>
+	)
+}
+
+const useNavigation = () => {
+	const pathname = usePathname()
+	const searchParams = useSearchParams()
+
+	const [currentRoute, setCurrentRoute] = useState<string | null>(null)
+	const [previousRoute, setPreviousRoute] = useState<string | null>(null)
+
+	useEffect(() => {
+		const url = `${pathname}?${searchParams}`
+		setPreviousRoute(currentRoute)
+		setCurrentRoute(url)
+	}, [pathname, searchParams])
+
+	return { previousRoute }
+}
+
+const NavigationContext = createContext<ReturnType<typeof useNavigation>>({
+	previousRoute: null
+})
