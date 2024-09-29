c2e2b63aef3e8ce53f578c3c29c175b0651a92d7
nfdmitry <khv1702@gmail.com>
2024-09-30 00:19:53 +0300

    Added module

---
+++ server/src/email/email.module.ts
@@ -0,0 +1,18 @@
+import { getMailerConfig } from '@/config/mailer.config'
+import { EmailService } from '@/email/email.service'
+import { MailerModule } from '@nestjs-modules/mailer'
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+
+@Module({
+	imports: [
+		MailerModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: getMailerConfig,
+			inject: [ConfigService]
+		})
+	],
+	providers: [EmailService],
+	exports: [EmailService]
+})
+export class EmailModule {}
