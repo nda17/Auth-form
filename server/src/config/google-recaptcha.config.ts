db7cc23f9011a564ec52b63f0291dff1d750fd45
nfdmitry <khv1702@gmail.com>
2024-09-29 23:31:01 +0300

    Added config

---
+++ server/src/config/google-recaptcha.config.ts
@@ -0,0 +1,12 @@
+import { isDev } from '@/utils/is-dev.util'
+import { ConfigService } from '@nestjs/config'
+import { GoogleRecaptchaModuleOptions } from '@nestlab/google-recaptcha/interfaces/google-recaptcha-module-options'
+
+export const getGoogleRecaptchaConfig = async (
+	configService: ConfigService
+): Promise<GoogleRecaptchaModuleOptions> => ({
+	secretKey: configService.get<string>('RECAPTCHA_SECRET_KEY'),
+	response: (req) => req.headers.recaptcha,
+	skipIf: () => isDev(configService)
+	// debug: isDev(configService)
+})
