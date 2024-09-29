967f28acdbb17abf1ba71706cad5617d15e9f498
nfdmitry <khv1702@gmail.com>
2024-09-30 00:15:11 +0300

    Added controller

---
+++ server/src/user/user.controller.ts
@@ -0,0 +1,34 @@
+import { Auth } from '@/auth/decorators/auth.decorator'
+import { CurrentUser } from '@/auth/decorators/user.decorator'
+import { UserService } from '@/user/user.service'
+import { Controller, Get } from '@nestjs/common'
+import { Role } from '@prisma/client'
+
+@Controller('users')
+export class UserController {
+	constructor(private readonly userService: UserService) {}
+
+	@Auth()
+	@Get('profile')
+	async getProfile(@CurrentUser('id') id: string) {
+		return this.userService.getById(id)
+	}
+
+	@Auth(Role.PREMIUM)
+	@Get('premium')
+	async getPremium() {
+		return { access: true }
+	}
+
+	@Auth([Role.ADMIN, Role.MANAGER])
+	@Get('manager')
+	async getManager() {
+		return { access: true }
+	}
+
+	@Auth(Role.ADMIN)
+	@Get('users-list')
+	async getList() {
+		return this.userService.getUsers()
+	}
+}
