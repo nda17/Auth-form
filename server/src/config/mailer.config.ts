850494201fd9b4b4126bc15defcba56040c65e93
nfdmitry <khv1702@gmail.com>
2024-10-01 00:01:36 +0300

    Added config

---
+++ server/src/config/mailer.config.ts
@@ -0,0 +1,20 @@
+import { isDev } from '@/utils/is-dev.util'
+import { MailerOptions } from '@nestjs-modules/mailer'
+import { ConfigService } from '@nestjs/config'
+
+export const getMailerConfig = async (
+	configService: ConfigService
+): Promise<MailerOptions> => ({
+	transport: {
+		host: configService.get('SMTP_SERVER'),
+		port: isDev(configService) ? 587 : 465,
+		secure: !isDev(configService),
+		auth: {
+			user: configService.get('SMTP_LOGIN'),
+			pass: configService.get('SMTP_PASSWORD')
+		}
+	},
+	defaults: {
+		from: '"auth-form" <no-reply@auth-form.ru>'
+	}
+})
