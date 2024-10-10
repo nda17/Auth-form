5e06a46b3668928e17cbadf3346f987070c5b141
nfdmitry <khv1702@gmail.com>
2024-10-11 01:40:13 +0300

    Added new path

--- client/src/config/pages/public.config.ts
+++ client/src/config/pages/public.config.ts
@@ -2,6 +2,7 @@ class PublicPages {
 	HOME = '/'
 	LOGIN = '/login'
 	REGISTER = '/register'
+	RESTORE_PASSWORD = '/restore-password'
 	FREE_CONTENT = '/free-content'
 	PREMIUM_CONTENT_PRICE = '/premium-content-price'
 }
