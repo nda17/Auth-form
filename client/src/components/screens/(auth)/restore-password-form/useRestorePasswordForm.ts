a99281dea2b3ee8e952cf58cf3af92c523d53509
nfdmitry <khv1702@gmail.com>
2024-10-11 02:00:26 +0300

    Added use restore password form logic

---
+++ client/src/components/screens/(auth)/restore-password-form/useRestorePasswordForm.ts
@@ -0,0 +1,65 @@
+import authService from '@/services/auth/auth.service'
+import { IEmail } from '@/shared/types/form.types'
+import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+import { useRef, useTransition } from 'react'
+import ReCAPTCHA from 'react-google-recaptcha'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import toast from 'react-hot-toast'
+
+const useRestorePasswordForm = () => {
+	const { register, handleSubmit, reset } = useForm<IEmail>()
+
+	const router = useRouter()
+
+	const [isPending, startTransition] = useTransition()
+
+	const recaptchaRef = useRef<ReCAPTCHA>(null)
+
+	const { mutate: mutateRestorePassword, isPending: isRestorePending } =
+		useMutation({
+			mutationKey: ['restore-password'],
+			mutationFn: (data: IEmail) =>
+				authService.getRestorePassword(
+					data,
+					recaptchaRef?.current?.getValue()
+				),
+			onSuccess() {
+				startTransition(() => {
+					toast.success('Temporary password sent by email')
+					reset()
+					router.replace('/login')
+				})
+			},
+			onError(error) {
+				if (axios.isAxiosError(error)) {
+					toast.error(error.response?.data?.message)
+					recaptchaRef.current.reset()
+				}
+			}
+		})
+
+	const onSubmit: SubmitHandler<IEmail> = (data) => {
+		const token = recaptchaRef?.current?.getValue()
+
+		if (!token) {
+			toast.error('Pass the captcha!')
+			return
+		}
+
+		mutateRestorePassword(data)
+	}
+
+	const isLoading = isPending || isRestorePending
+
+	return {
+		register,
+		handleSubmit,
+		onSubmit,
+		recaptchaRef,
+		isLoading
+	}
+}
+
+export default useRestorePasswordForm
