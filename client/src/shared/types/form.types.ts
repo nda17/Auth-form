be0fbb4dd3650e3c6f80ecf86159ea482971f16e
nfdmitry <khv1702@gmail.com>
2024-10-11 01:41:45 +0300

    Added types

--- client/src/shared/types/form.types.ts
+++ client/src/shared/types/form.types.ts
@@ -3,3 +3,5 @@ import { IUser } from '@/shared/types/user.types'
 export interface IFormData extends Pick<IUser, 'email'> {
 	password: string
 }
+
+export interface IEmail extends Pick<IUser, 'email'> {}
