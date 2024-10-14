b2c1532fb526ef6a260da2bfe69b13ae25052a0f
nfdmitry <khv1702@gmail.com>
2024-10-14 04:16:00 +0300

    Added validators

--- server/src/auth/dto/auth.dto.ts
+++ server/src/auth/dto/auth.dto.ts
@@ -1,11 +1,18 @@
-import { IsEmail, IsString, MinLength } from 'class-validator'
+import { IsEmail, IsString, Matches } from 'class-validator'
 
 export class AuthDto {
+	@Matches(
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+		{
+			message: 'Please enter a valid email'
+		}
+	)
 	@IsEmail()
 	email: string
 
-	@MinLength(6, {
-		message: 'Password must be at least 6 characters long'
+	@Matches(/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}/g, {
+		message:
+			'Min length should more 6 symbols. Contains 1 number 0-9, 1 Latin letter a-z, 1 Latin letter A-Z'
 	})
 	@IsString()
 	password: string
