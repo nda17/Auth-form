431091228f68c051fd2e9991826597a6fe0a31a0
nfdmitry <khv1702@gmail.com>
2024-10-22 01:57:26 +0300

    Refactor

--- client/src/components/layout/nav-menu/desktop/desktop-dynamic-menu/DesktopDynamicMenu.tsx
+++ client/src/components/layout/nav-menu/desktop/desktop-dynamic-menu/DesktopDynamicMenu.tsx
@@ -1,40 +1,48 @@
+'use client'
 import styles from '@/components/layout/nav-menu/desktop/desktop-dynamic-menu/DesktopDynamicMenu.module.scss'
+import LogoutButton from '@/components/layout/nav-menu/logout-button/LogoutButton'
 import MaterialIcon from '@/components/ui/icons/MaterialIcon'
 import useProfile from '@/hooks/useProfile'
-import { UserRole } from '@/services/auth/auth.types'
 import clsx from 'clsx'
 import { NextPage } from 'next'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 const DesktopDynamicMenu: NextPage = () => {
 	const { user } = useProfile()
-	const isLoggedIn = user?.isLoggedIn
-	const isManager =
-		user?.isManager && user?.rights?.includes(UserRole.MANAGER)
-	const isAdmin = user?.isAdmin && user?.rights?.includes(UserRole.ADMIN)
+	const [navState, setNavState] = useState(true)
+
+	useEffect(() => {}, [user, navState])
 
 	return (
 		<div className={styles.wrapper}>
-			{isLoggedIn && (
+			{user?.isLoggedIn && (
 				<Link href="/profile" className={clsx(styles['link-button'])}>
 					<MaterialIcon name="MdSettings" fill="blue" />
 					Profile
 				</Link>
 			)}
-
-			{isLoggedIn && isManager && (
+			{user?.isManager && (
 				<Link href="/manager" className={clsx(styles['link-button'])}>
 					<MaterialIcon name="MdGroup" fill="green" />
 					Manager
 				</Link>
 			)}
-
-			{isLoggedIn && isAdmin && (
+			{user?.isAdmin && (
 				<Link href="/admin" className={clsx(styles['link-button'])}>
 					<MaterialIcon name="MdOutlineLock" fill="green" />
 					Admin
 				</Link>
 			)}
+
+			{!user?.isLoggedIn && (
+				<Link href="/login" className={clsx(styles['link-auth-button'])}>
+					<MaterialIcon name="MdLogout" fill="green" />
+					Login
+				</Link>
+			)}
+
+			{user?.isLoggedIn && <LogoutButton setNavState={setNavState} />}
 		</div>
 	)
 }
