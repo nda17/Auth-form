2df90defe9833e8a9aec6cb90ab142e1bf2ef1fc
nfdmitry <khv1702@gmail.com>
2024-10-22 01:56:22 +0300

    Deleted redux-store provider

--- client/src/providers/Main-provider/MainProvider.tsx
+++ client/src/providers/Main-provider/MainProvider.tsx
@@ -1,12 +1,10 @@
 'use client'
 import { IMainProvider } from '@/providers/main-provider/main-provider.interface'
 import { NavigationProvider } from '@/providers/navigation-provider/NavigationProvider'
-import { store } from '@/store/store'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { NextPage } from 'next'
 import { Toaster } from 'react-hot-toast'
-import { Provider } from 'react-redux'
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -18,13 +16,11 @@ const queryClient = new QueryClient({
 
 const MainProvider: NextPage<IMainProvider> = ({ children }) => {
 	return (
-		<Provider store={store}>
-			<QueryClientProvider client={queryClient}>
-				<Toaster />
-				<NavigationProvider>{children}</NavigationProvider>
-				<ReactQueryDevtools initialIsOpen={false} />
-			</QueryClientProvider>
-		</Provider>
+		<QueryClientProvider client={queryClient}>
+			<Toaster />
+			<NavigationProvider>{children}</NavigationProvider>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	)
 }
 
