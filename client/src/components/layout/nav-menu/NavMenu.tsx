08e06224421d1a5e37e11a96cb37ae4e9f8dc987
nfdmitry <khv1702@gmail.com>
2024-10-22 02:19:17 +0300

    Refactor

--- client/src/components/layout/nav-menu/NavMenu.tsx
+++ client/src/components/layout/nav-menu/NavMenu.tsx
@@ -1,43 +1,29 @@
+'use client'
 import styles from '@/components/layout/nav-menu/NavMenu.module.scss'
-import DesktopDynamicMenu from '@/components/layout/nav-menu/desktop/desktop-dynamic-menu/DesktopDynamicMenu'
 import DesktopStaticMenu from '@/components/layout/nav-menu/desktop/desktop-static-menu/DesktopStaticMenu'
-import LogoutButton from '@/components/layout/nav-menu/logout-button/LogoutButton'
-import MaterialIcon from '@/components/ui/icons/MaterialIcon'
-import useAuth from '@/hooks/useAuth'
-import { setAuthStatus } from '@/store/auth-status/auth-status.slice'
-import clsx from 'clsx'
+import SkeletonLoader from '@/components/ui/skeleton-loader/SkeletonLoader'
 import { NextPage } from 'next'
-import Link from 'next/link'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import dynamic from 'next/dynamic'
+const DesktopDynamicMenu = dynamic(
+	() =>
+		import(
+			'@/components/layout/nav-menu/desktop/desktop-dynamic-menu/DesktopDynamicMenu'
+		),
+	{
+		loading: () => (
+			<div className="w-[78.453px] h-full flex flex-col justify-center">
+				<SkeletonLoader count={1} className="w-full h-4" />
+			</div>
+		),
+		ssr: false
+	}
+)
 
 const NavMenu: NextPage = () => {
-	const { statusAuth } = useAuth()
-
-	const isLoggedIn = useSelector(
-		(state: any) => state.authStatus.isLoggedIn
-	)
-
-	const dispatch = useDispatch()
-
-	useEffect(() => {
-		dispatch(setAuthStatus(statusAuth ? true : false))
-	}, [statusAuth])
-
 	return (
 		<div className={styles.wrapper}>
 			<DesktopStaticMenu />
-			{isLoggedIn && <DesktopDynamicMenu />}
-			<div className={styles.wrapper}>
-				{!isLoggedIn ? (
-					<Link href="/login" className={clsx(styles['link-auth-button'])}>
-						<MaterialIcon name="MdLogout" fill="green" />
-						Login
-					</Link>
-				) : (
-					<LogoutButton />
-				)}
-			</div>
+			<DesktopDynamicMenu />
 		</div>
 	)
 }
