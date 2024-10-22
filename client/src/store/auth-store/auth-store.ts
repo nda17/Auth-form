be5897b4038ca595979b953dadff4304acb18366
nfdmitry <khv1702@gmail.com>
2024-10-22 06:08:27 +0300

    Added auth store

---
+++ client/src/store/auth-store/auth-store.ts
@@ -0,0 +1,7 @@
+import { IAuthStore } from '@/store/auth-store/auth-store.interface'
+import { create } from 'zustand'
+
+export const useAuthStore = create<IAuthStore>((set) => ({
+	auth: false,
+	setAuth: () => set((state) => ({ auth: !state.auth }))
+}))
