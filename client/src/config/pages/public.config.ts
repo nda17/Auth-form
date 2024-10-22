61d8787eb9fa37ffeffad359319471c2a24a9d74
nfdmitry <khv1702@gmail.com>
2024-10-22 13:47:09 +0300

    Added links

--- client/src/config/pages/public.config.ts
+++ client/src/config/pages/public.config.ts
@@ -3,7 +3,10 @@ class PublicPages {
 	LOGIN = '/login'
 	REGISTER = '/register'
 	RESTORE_PASSWORD = '/restore-password'
+	USER_PROFILE = '/profile'
+	MANAGER = '/manager'
 	FREE_CONTENT = '/free-content'
+	PREMIUM_CONTENT = '/premium-content'
 	PRIVACY_POLICY = '/legal-documentation/privacy-policy'
 	COOKIE_NOTICE = '/legal-documentation/cookie-notice'
 }
