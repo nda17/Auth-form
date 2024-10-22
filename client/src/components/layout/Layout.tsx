299161f72e1869b5e33cedb73aaf5a3aec55f6bb
nfdmitry <khv1702@gmail.com>
2024-10-23 01:30:34 +0300

    Added state

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -1,17 +1,23 @@
+'use client'
 import styles from '@/components/layout/Layout.module.scss'
 import Footer from '@/components/layout/footer/Footer'
 import Header from '@/components/layout/header/Header'
 import { ILayout } from '@/components/layout/layout.interface'
 import VeilBackground from '@/components/ui/veil-background/VeilBackground'
+import { useVeilBackgroundStore } from '@/store/veil-background-store/veil-background-store'
 import { NextPage } from 'next'
 
 const Layout: NextPage<ILayout> = ({ children }) => {
+	const visibleVeilBackground = useVeilBackgroundStore(
+		(state) => state.visible
+	)
+
 	return (
 		<div className={styles.layout}>
 			<Header />
 			<main className={styles.main}>
 				{children}
-				<VeilBackground />
+				{visibleVeilBackground && <VeilBackground />}
 			</main>
 			<Footer />
 		</div>
