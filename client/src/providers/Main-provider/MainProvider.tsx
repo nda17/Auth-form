47a5d74ef382897ecf2968d844c9d0bc35d62a56
nfdmitry <khv1702@gmail.com>
2024-10-22 18:08:19 +0300

    Added cookie consent provider

--- client/src/providers/Main-provider/MainProvider.tsx
+++ client/src/providers/Main-provider/MainProvider.tsx
@@ -1,4 +1,5 @@
 'use client'
+import CookieConsentProvider from '@/providers/cookie-consent-provider/CookieConsentProvider'
 import { IMainProvider } from '@/providers/main-provider/main-provider.interface'
 import { NavigationProvider } from '@/providers/navigation-provider/NavigationProvider'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -18,6 +19,7 @@ const MainProvider: NextPage<IMainProvider> = ({ children }) => {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<Toaster />
+			<CookieConsentProvider />
 			<NavigationProvider>{children}</NavigationProvider>
 			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
