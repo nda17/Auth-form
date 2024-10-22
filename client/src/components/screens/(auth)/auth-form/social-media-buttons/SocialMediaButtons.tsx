302393839ad12eb017dca0093d153dbc702e6d4e
nfdmitry <khv1702@gmail.com>
2024-10-22 15:52:50 +0300

    Refactor

--- client/src/components/screens/(auth)/auth-form/social-media-buttons/SocialMediaButtons.tsx
+++ client/src/components/screens/(auth)/auth-form/social-media-buttons/SocialMediaButtons.tsx
@@ -1,5 +1,5 @@
 import styles from '@/components/screens/(auth)/auth-form/social-media-buttons/SocialMediaButtons.module.scss'
-import Image from 'next/image'
+import FontAwesomeIcon from '@/components/ui/icons/FontAwesomeIcon'
 import { useRouter } from 'next/navigation'
 
 const SocialMediaButtons = () => {
@@ -12,26 +12,14 @@ const SocialMediaButtons = () => {
 				className={styles.button}
 				type="button"
 			>
-				<Image
-					src="/google.svg"
-					width={20}
-					height={20}
-					alt="google auth"
-					className={styles.image}
-				/>
+				<FontAwesomeIcon name="FaGoogle" fill="#000000" />
 			</button>
 			<button
 				onClick={() => router.push('/auth/github')}
 				className={styles.button}
 				type="button"
 			>
-				<Image
-					src="/github.svg"
-					width={20}
-					height={20}
-					alt="github auth"
-					className={styles.image}
-				/>
+				<FontAwesomeIcon name="FaGithub" fill="#000000" />
 			</button>
 		</div>
 	)
