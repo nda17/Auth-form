d31dc402888d4406329a58acd09b3451dc64253a
nfdmitry <khv1702@gmail.com>
2024-10-22 20:01:12 +0300

    Added hamburger component

---
+++ client/src/components/ui/hamburger/Hamburger.tsx
@@ -0,0 +1,28 @@
+import styles from '@/components/ui/hamburger/Hamburger.module.scss'
+import MaterialIcon from '@/components/ui/icons/MaterialIcon'
+import { useHamburgerStore } from '@/store/hamburger-store/hamburger-store'
+import { NextPage } from 'next'
+
+const Hamburger: NextPage = () => {
+	const visibleHamburger = useHamburgerStore((state) => state.visible)
+	const changeVisibleHamburger = useHamburgerStore(
+		(state) => state.setVisible
+	)
+
+	return (
+		<div
+			className={styles.hamburger}
+			onClick={() => {
+				changeVisibleHamburger()
+			}}
+		>
+			{visibleHamburger ? (
+				<MaterialIcon name={'MdClose'} fill="#fc0303" />
+			) : (
+				<MaterialIcon name={'MdMenu'} />
+			)}
+		</div>
+	)
+}
+
+export default Hamburger
