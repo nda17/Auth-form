6018b03993a9b4772fcf4bdc0c5c3b7718143706
nfdmitry <khv1702@gmail.com>
2024-10-22 20:10:00 +0300

    Added desktop navbar component

---
+++ client/src/components/layout/nav-menu/desktop/DesktopNavBar.tsx
@@ -0,0 +1,30 @@
+import styles from '@/components/layout/nav-menu/desktop/DesktopNavBar.module.scss'
+import DesktopStaticMenu from '@/components/layout/nav-menu/desktop/desktop-static-menu/DesktopStaticMenu'
+import SkeletonLoader from '@/components/ui/skeleton-loader/SkeletonLoader'
+import { NextPage } from 'next'
+import dynamic from 'next/dynamic'
+const DesktopDynamicMenu = dynamic(
+	() =>
+		import(
+			'@/components/layout/nav-menu/desktop/desktop-dynamic-menu/DesktopDynamicMenu'
+		),
+	{
+		loading: () => (
+			<div className="w-[78.453px] h-full flex flex-col justify-center">
+				<SkeletonLoader count={1} className="w-full h-4" />
+			</div>
+		),
+		ssr: false
+	}
+)
+
+const DesktopNavBar: NextPage = () => {
+	return (
+		<div className={styles.wrapper}>
+			<DesktopStaticMenu />
+			<DesktopDynamicMenu />
+		</div>
+	)
+}
+
+export default DesktopNavBar
