bfb08026df8b3d0bb785afa36b32b4611d446208
nfdmitry <khv1702@gmail.com>
2024-10-22 11:54:32 +0300

    Added pagination list users

--- client/src/components/screens/admin/Admin.tsx
+++ client/src/components/screens/admin/Admin.tsx
@@ -2,12 +2,44 @@
 import styles from '@/components/screens/admin/Admin.module.scss'
 import CirclesLoader from '@/components/ui/circles-loader/CirclesLoader'
 import Heading from '@/components/ui/heading/Heading'
+import Pagination from '@/components/ui/pagination/Pagination'
 import SubHeading from '@/components/ui/sub-heading/SubHeading'
-import useAdmin from '@/hooks/useAdmin'
+import useUsersList from '@/hooks/useUsersList'
 import { NextPage } from 'next'
+import { useState } from 'react'
 
 const Admin: NextPage = () => {
-	const { data, isLoading } = useAdmin()
+	const { data, isLoading } = useUsersList()
+
+	//Pagination settings
+	const [currentPage, setCurrentPage] = useState(1)
+	const itemQuantity = 4
+	const lastCardIndex = currentPage * itemQuantity
+	const firstCardIndex = lastCardIndex - itemQuantity
+	const activePage = data?.slice(firstCardIndex, lastCardIndex)
+	const listPage = []
+	for (let i = 1; i <= Math.ceil(data?.length / itemQuantity); i++) {
+		listPage.push(i)
+	}
+
+	const prevPage = () => {
+		if (currentPage !== 1) {
+			setCurrentPage((prev) => prev - 1)
+		} else {
+			return
+		}
+	}
+
+	const changeActivePage = (activePage: number) =>
+		setCurrentPage(activePage)
+
+	const nextPage = () => {
+		if (currentPage !== Math.ceil(data?.length / itemQuantity)) {
+			setCurrentPage((prev) => prev + 1)
+		} else {
+			return
+		}
+	}
 
 	return (
 		<div className={styles.wrapper}>
@@ -15,12 +47,21 @@ const Admin: NextPage = () => {
 			<SubHeading text="List users:" />
 			{isLoading ? (
 				<CirclesLoader />
-			) : data ? (
-				data.map((user) => (
-					<div key={user.id} className={styles.user}>
-						{user.email}
-					</div>
-				))
+			) : data?.length ? (
+				<div>
+					{activePage.map((user) => (
+						<div key={user.id} className={styles.user}>
+							{user.email}
+						</div>
+					))}
+					<Pagination
+						listPage={listPage}
+						currentPage={currentPage}
+						prevPage={prevPage}
+						nextPage={nextPage}
+						changeActivePage={changeActivePage}
+					/>
+				</div>
 			) : (
 				<p>Not found!</p>
 			)}
