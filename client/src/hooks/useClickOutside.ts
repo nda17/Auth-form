94b255c1768871f4019e7a9b413d00e87217e086
nfdmitry <khv1702@gmail.com>
2024-10-22 20:03:32 +0300

    Added hook

---
+++ client/src/hooks/useClickOutside.ts
@@ -0,0 +1,22 @@
+import { useEffect } from 'react'
+
+const useClickOutside = (
+	ref: React.MutableRefObject<HTMLElement | null>,
+	callback: () => void
+) => {
+	const handleClick = (event: MouseEvent) => {
+		if (ref.current && !ref.current.contains(event.target as Node)) {
+			callback()
+		}
+	}
+
+	useEffect(() => {
+		document.addEventListener('mouseup', handleClick)
+
+		return () => {
+			document.removeEventListener('mouseup', handleClick)
+		}
+	}, [])
+}
+
+export { useClickOutside }
