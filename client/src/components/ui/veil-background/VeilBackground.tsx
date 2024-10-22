8a759aba323fc364fbe1a2ca81dc34dedc29d020
nfdmitry <khv1702@gmail.com>
2024-10-22 23:43:36 +0300

    Added veil background component

---
+++ client/src/components/ui/veil-background/VeilBackground.tsx
@@ -0,0 +1,8 @@
+import styles from '@/components/ui/veil-background/VeilBackground.module.scss'
+import { NextPage } from 'next'
+
+const VeilBackground: NextPage = () => {
+	return <div className={styles.background}></div>
+}
+
+export default VeilBackground
