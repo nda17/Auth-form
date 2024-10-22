fc462ff856863aeac8c4d8ef55d37b2ead2b42e1
nfdmitry <khv1702@gmail.com>
2024-10-23 01:11:25 +0300

    Added veil background

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -2,13 +2,17 @@ import styles from '@/components/layout/Layout.module.scss'
 import Footer from '@/components/layout/footer/Footer'
 import Header from '@/components/layout/header/Header'
 import { ILayout } from '@/components/layout/layout.interface'
+import VeilBackground from '@/components/ui/veil-background/VeilBackground'
 import { NextPage } from 'next'
 
 const Layout: NextPage<ILayout> = ({ children }) => {
 	return (
 		<div className={styles.layout}>
 			<Header />
-			<main className={styles.main}>{children}</main>
+			<main className={styles.main}>
+				{children}
+				<VeilBackground />
+			</main>
 			<Footer />
 		</div>
 	)
