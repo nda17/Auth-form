205b6f00d88af78d8237c5613b6f34a60b39ddde
nfdmitry <khv1702@gmail.com>
2024-10-22 15:30:42 +0300

    Added paths

--- client/src/config/pages/public.config.ts
+++ client/src/config/pages/public.config.ts
@@ -9,6 +9,8 @@ class PublicPages {
 	PREMIUM_CONTENT = '/premium-content'
 	PRIVACY_POLICY = '/legal-documentation/privacy-policy'
 	COOKIE_NOTICE = '/legal-documentation/cookie-notice'
+	SOCIALS_LINK_VK = 'https://vk.ru'
+	SOCIALS_LINK_TG = 'https://web.telegram.org/k/'
 }
 
 export const PUBLIC_PAGES = new PublicPages()
