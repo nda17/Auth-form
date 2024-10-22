85c8f4701df649a6e07a08ec16a72e0b2c841835
nfdmitry <khv1702@gmail.com>
2024-10-22 18:04:48 +0300

    Added cookie consent provider

---
+++ client/src/providers/cookie-consent-provider/CookieConsentProvider.tsx
@@ -0,0 +1,21 @@
+import CookieConsentPopup from '@/components/ui/cookie-consent-popup/CookieConsentPopup'
+import Cookies from 'js-cookie'
+import { NextPage } from 'next'
+import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+const CookieConsentProvider: NextPage = () => {
+	const [confirm, setConfirm] = useState(true)
+	const pathname = usePathname()
+
+	const getCookieConsent = () => {
+		Cookies.get('cookieConsent') ? setConfirm(true) : setConfirm(false)
+	}
+
+	useEffect(() => {
+		getCookieConsent()
+	}, [confirm, pathname])
+	return <CookieConsentPopup status={!confirm ? 'show' : 'hide'} />
+}
+
+export default CookieConsentProvider
