a467aed2b9720c8ff7115b779d0c229e4d037a1e
nfdmitry <khv1702@gmail.com>
2024-10-24 00:53:05 +0300

    Deleted file

--- client/src/store/veil-background-store/veil-background-store.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface IVeilBackgroundStore {
-	visible: boolean
-	setVisible: () => void
-}
--- client/src/store/veil-background-store/veil-background-store.ts
+++
@@ -1,9 +0,0 @@
-import { IVeilBackgroundStore } from '@/store/veil-background-store/veil-background-store.interface'
-import { create } from 'zustand'
-
-export const useVeilBackgroundStore = create<IVeilBackgroundStore>(
-	(set) => ({
-		visible: false,
-		setVisible: () => set((state) => ({ visible: !state.visible }))
-	})
-)
