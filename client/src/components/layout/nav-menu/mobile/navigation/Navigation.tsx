794195c27c089251fdddd654c3f573db61247b50
nfdmitry <khv1702@gmail.com>
2024-10-24 01:09:37 +0300

    Added navigation component

---
+++ client/src/components/layout/nav-menu/mobile/navigation/Navigation.tsx
@@ -0,0 +1,18 @@
+import styles from '@/components/layout/nav-menu/mobile/navigation/Navigation.module.scss'
+import Hamburger from '@/components/ui/hamburger/Hamburger'
+import LogoImage from '@/components/ui/logo-image/LogoImage'
+import clsx from 'clsx'
+import { NextPage } from 'next'
+
+const Navigation: NextPage = () => {
+	return (
+		<header className={styles.wrapper}>
+			<div className={clsx(styles['layout-container'])}>
+				<LogoImage />
+				<Hamburger />
+			</div>
+		</header>
+	)
+}
+
+export default Navigation
