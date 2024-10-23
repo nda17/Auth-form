22b2eb81f5d7f12a2468510f3c76167637414356
nfdmitry <khv1702@gmail.com>
2024-10-24 01:07:25 +0300

    Refactor

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -1,24 +1,14 @@
-'use client'
 import styles from '@/components/layout/Layout.module.scss'
 import Footer from '@/components/layout/footer/Footer'
 import Header from '@/components/layout/header/Header'
 import { ILayout } from '@/components/layout/layout.interface'
-import VeilBackground from '@/components/ui/veil-background/VeilBackground'
-import { useVeilBackgroundStore } from '@/store/veil-background-store/veil-background-store'
 import { NextPage } from 'next'
 
 const Layout: NextPage<ILayout> = ({ children }) => {
-	const visibleVeilBackground = useVeilBackgroundStore(
-		(state) => state.visible
-	)
-
 	return (
 		<div className={styles.layout}>
 			<Header />
-			<main className={styles.main}>
-				{children}
-				{visibleVeilBackground && <VeilBackground />}
-			</main>
+			<main className={styles.main}>{children}</main>
 			<Footer />
 		</div>
 	)
