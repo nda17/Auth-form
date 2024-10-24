b6bae787a68e5f682e57702540869092d00d89c4
nfdmitry <khv1702@gmail.com>
2024-10-24 05:37:20 +0300

    Added auth items component

---
+++ client/src/components/layout/nav-menu/desktop/menu/auth-items/AuthItems.tsx
@@ -0,0 +1,58 @@
+import LogoutButton from '@/components/layout/nav-menu/desktop/menu/logout-button/LogoutButton'
+import MenuItem from '@/components/layout/nav-menu/desktop/menu/menu-item/MenuItem'
+import { ADMIN_PAGES } from '@/config/pages/admin.config'
+import { PUBLIC_PAGES } from '@/config/pages/public.config'
+import useUser from '@/hooks/useUser'
+import { NextPage } from 'next'
+
+const AuthItems: NextPage = () => {
+	const { user } = useUser()
+
+	return (
+		<>
+			{user?.isLoggedIn && (
+				<MenuItem
+					item={{
+						icon: 'MdSettings',
+						link: PUBLIC_PAGES.USER_PROFILE,
+						title: 'Profile'
+					}}
+				/>
+			)}
+
+			{user?.isManager && (
+				<MenuItem
+					item={{
+						icon: 'MdGroup',
+						link: PUBLIC_PAGES.MANAGER,
+						title: 'Manager'
+					}}
+				/>
+			)}
+
+			{user?.isAdmin && (
+				<MenuItem
+					item={{
+						icon: 'MdOutlineLock',
+						link: ADMIN_PAGES.HOME,
+						title: 'Admin'
+					}}
+				/>
+			)}
+
+			{!user?.isLoggedIn && (
+				<MenuItem
+					item={{
+						icon: 'MdLogout',
+						link: PUBLIC_PAGES.LOGIN,
+						title: 'Login'
+					}}
+				/>
+			)}
+
+			{user?.isLoggedIn && <LogoutButton />}
+		</>
+	)
+}
+
+export default AuthItems
