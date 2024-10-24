55f95b6a47a643f59d58d5435cbc4053d88310d5
nfdmitry <khv1702@gmail.com>
2024-10-24 05:38:50 +0300

    Added menu item component

---
+++ client/src/components/layout/nav-menu/desktop/menu/menu-item/MenuItem.tsx
@@ -0,0 +1,26 @@
+import styles from '@/components/layout/nav-menu/desktop/menu/menu-item/MenuItem.module.scss'
+import { IMenuItem } from '@/components/layout/nav-menu/desktop/menu/menu-item/menu-item.interface'
+import MaterialIcon from '@/components/ui/icons/MaterialIcon'
+import clsx from 'clsx'
+import { NextPage } from 'next'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const MenuItem: NextPage<{ item: IMenuItem }> = ({ item }) => {
+	const pathname = usePathname()
+
+	return (
+		<li
+			className={clsx([styles.wrapper], {
+				[styles.active]: pathname === item.link
+			})}
+		>
+			<Link href={item.link} className={clsx(styles['link-button'])}>
+				<MaterialIcon name={item.icon} />
+				{item.title}
+			</Link>
+		</li>
+	)
+}
+
+export default MenuItem
