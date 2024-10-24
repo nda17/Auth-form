fb3605f90fcf0289ff7c135ac009c93ab7788403
nfdmitry <khv1702@gmail.com>
2024-10-25 02:14:44 +0300

    Added hook

---
+++ client/src/hooks/useCountUsers.ts
@@ -0,0 +1,13 @@
+import adminService from '@/services/admin/admin.service'
+import { useQuery } from '@tanstack/react-query'
+
+const useCountUsers = () => {
+	const { data: response, isLoading } = useQuery({
+		queryKey: ['get-count-users'],
+		queryFn: () => adminService.fetchCountUsers()
+	})
+
+	return { response, isLoading }
+}
+
+export default useCountUsers
