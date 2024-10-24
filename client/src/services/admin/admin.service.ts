3c9be5ce3d60adf79e2a79925e5b677e74abc5b2
nfdmitry <khv1702@gmail.com>
2024-10-25 02:16:10 +0300

    Added admin service

---
+++ client/src/services/admin/admin.service.ts
@@ -0,0 +1,11 @@
+import { axiosInterceptorsRequest } from '@/api/interceptors'
+
+class AdminService {
+	private _BASE_URL = '/users'
+
+	async fetchCountUsers() {
+		return axiosInterceptorsRequest.get<number>(`${this._BASE_URL}/count`)
+	}
+}
+
+export default new AdminService()
