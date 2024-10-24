e2d9b9b46fb2d8c43ee17b1b3fee7590e704f4b6
nfdmitry <khv1702@gmail.com>
2024-10-25 02:11:48 +0300

    Added statistics component

---
+++ client/src/components/screens/admin/statistics/Statistics.tsx
@@ -0,0 +1,8 @@
+import CountUsers from '@/components/screens/admin/statistics/count-users/CountUsers'
+import { NextPage } from 'next'
+
+const Statistics: NextPage = () => {
+	return <CountUsers />
+}
+
+export default Statistics
