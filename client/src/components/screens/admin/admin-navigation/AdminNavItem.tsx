1111b8075e47137a707f591a673a2c2866ae4dfe
nfdmitry <khv1702@gmail.com>
2024-10-25 02:11:02 +0300

    Added admin nav item component

---
+++ client/src/components/screens/admin/admin-navigation/AdminNavItem.tsx
@@ -0,0 +1,29 @@
+import styles from '@/components/screens/admin/admin-navigation/AdminNavigation.module.scss'
+import { INavItem } from '@/components/screens/admin/data/admin-navigation.interface'
+import clsx from 'clsx'
+import { NextPage } from 'next'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const AdminNavItem: NextPage<{ item: INavItem }> = ({
+	item: { link, title, option }
+}) => {
+	const pathname = usePathname()
+
+	return (
+		<li>
+			<Link href={link}>
+				<span
+					className={clsx({
+						[styles.active]:
+							pathname === link || pathname.includes(String(option))
+					})}
+				>
+					{title}
+				</span>
+			</Link>
+		</li>
+	)
+}
+
+export default AdminNavItem
