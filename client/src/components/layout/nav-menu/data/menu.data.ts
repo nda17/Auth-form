f5093d7a54937d6d5053574d9dc7bcca7334a386
nfdmitry <khv1702@gmail.com>
2024-10-24 05:16:09 +0300

    Added list menu link

---
+++ client/src/components/layout/nav-menu/data/menu.data.ts
@@ -0,0 +1,22 @@
+import { IMenu } from '@/components/layout/nav-menu/desktop/menu/menu.interface'
+import { PUBLIC_PAGES } from '@/config/pages/public.config'
+
+export const staticMenu: IMenu = {
+	items: [
+		{
+			icon: 'MdHomeWork',
+			link: PUBLIC_PAGES.HOME,
+			title: 'Home'
+		},
+		{
+			icon: 'MdCheckCircle',
+			link: PUBLIC_PAGES.FREE_CONTENT,
+			title: 'Free content'
+		},
+		{
+			icon: 'MdPaid',
+			link: PUBLIC_PAGES.PREMIUM_CONTENT,
+			title: 'Premium content'
+		}
+	]
+}
