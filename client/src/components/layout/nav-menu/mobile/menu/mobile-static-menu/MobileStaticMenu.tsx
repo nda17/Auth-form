27eaf71c4532f73f5f06e5b11a5bb45173ee18e8
nfdmitry <khv1702@gmail.com>
2024-10-25 00:18:56 +0300

    Refactor

--- client/src/components/layout/nav-menu/mobile/menu/mobile-static-menu/MobileStaticMenu.tsx
+++ client/src/components/layout/nav-menu/mobile/menu/mobile-static-menu/MobileStaticMenu.tsx
@@ -1,47 +1,16 @@
+import { staticMenu } from '@/components/layout/nav-menu/data/menu.data'
+import MenuItem from '@/components/layout/nav-menu/mobile/menu/menu-item/MenuItem'
+import { IMenuItem } from '@/components/layout/nav-menu/mobile/menu/menu-item/menu-item.interface'
 import styles from '@/components/layout/nav-menu/mobile/menu/mobile-static-menu/MobileStaticMenu.module.scss'
-import MaterialIcon from '@/components/ui/icons/MaterialIcon'
-import { PUBLIC_PAGES } from '@/config/pages/public.config'
-import { useHamburgerStore } from '@/store/hamburger-store/hamburger-store'
-import clsx from 'clsx'
 import { NextPage } from 'next'
-import Link from 'next/link'
 
 const MobileStaticMenu: NextPage = () => {
-	const changeVisibleHamburger = useHamburgerStore(
-		(state) => state.setVisible
-	)
-
-	const closeMenu = () => {
-		changeVisibleHamburger()
-	}
-
 	return (
-		<div className={styles.wrapper}>
-			<Link
-				onClick={closeMenu}
-				href={PUBLIC_PAGES.HOME}
-				className={clsx(styles['link-button'])}
-			>
-				<MaterialIcon name="MdHomeWork" fill="orange" />
-				Home
-			</Link>
-			<Link
-				onClick={closeMenu}
-				href={PUBLIC_PAGES.FREE_CONTENT}
-				className={clsx(styles['link-button'])}
-			>
-				<MaterialIcon name="MdCheckCircle" fill="green" />
-				Free content
-			</Link>
-			<Link
-				onClick={closeMenu}
-				href={PUBLIC_PAGES.PREMIUM_CONTENT}
-				className={clsx(styles['link-button'])}
-			>
-				<MaterialIcon name="MdPaid" fill="red" />
-				Premium content
-			</Link>
-		</div>
+		<ul className={styles.wrapper}>
+			{staticMenu.items?.map((item: IMenuItem) => (
+				<MenuItem item={item} key={item.link} />
+			))}
+		</ul>
 	)
 }
 
