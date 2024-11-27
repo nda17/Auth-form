46135d110a99122a6009e184c44eac7f89d13ec3
nfdmitry <khv1702@gmail.com>
2024-11-28 01:22:12 +0300

    Update import paths

--- client/src/components/ui/admin/admin-navigation/AdminNavigation.tsx
+++ client/src/components/ui/admin/admin-navigation/AdminNavigation.tsx
@@ -1,6 +1,6 @@
-import AdminNavItem from '@/components/screens/admin/admin-navigation/AdminNavItem'
-import styles from '@/components/screens/admin/admin-navigation/AdminNavigation.module.scss'
-import { navItems } from '@/components/screens/admin/data/admin-navigation.data'
+import AdminNavItem from '@/components/ui/admin/admin-navigation/AdminNavItem'
+import styles from '@/components/ui/admin/admin-navigation/AdminNavigation.module.scss'
+import { navItems } from '@/components/ui/admin/admin-navigation/data/admin-navigation.data'
 import clsx from 'clsx'
 import { NextPage } from 'next'
 
