49d691375365998e977eb33a2042c19d26a3f36a
nfdmitry <khv1702@gmail.com>
2024-11-29 01:47:02 +0300

    Refactor

--- client/src/components/ui/hamburger/Hamburger.tsx
+++ client/src/components/ui/hamburger/Hamburger.tsx
@@ -1,21 +1,29 @@
 import styles from '@/components/ui/hamburger/Hamburger.module.scss'
 import MaterialIcon from '@/components/ui/icons/MaterialIcon'
 import { useHamburgerStore } from '@/store/hamburger-store/hamburger-store'
+import { useVeilBackgroundStore } from '@/store/veil-background-store/veil-background-store'
 import { NextPage } from 'next'
 
 const Hamburger: NextPage = () => {
+	const visibleVeilBackground = useVeilBackgroundStore(
+		(state) => state.visible
+	)
+	const changeVisibleVeilBackground = useVeilBackgroundStore(
+		(state) => state.setVisible
+	)
 	const visibleHamburger = useHamburgerStore((state) => state.visible)
 	const changeVisibleHamburger = useHamburgerStore(
 		(state) => state.setVisible
 	)
 
-	const closeMenu = () => {
+	const changeStateMenu = () => {
+		changeVisibleVeilBackground()
 		changeVisibleHamburger()
 	}
 
 	return (
-		<div className={styles.hamburger} onClick={closeMenu}>
-			{visibleHamburger ? (
+		<div className={styles.hamburger} onClick={changeStateMenu}>
+			{visibleVeilBackground && visibleHamburger ? (
 				<MaterialIcon name={'MdClose'} fill="#fc0303" />
 			) : (
 				<MaterialIcon name={'MdMenu'} />
