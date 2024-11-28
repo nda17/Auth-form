c1228d29bf0c27d09747fedb67544c8982671f5d
nfdmitry <khv1702@gmail.com>
2024-11-28 21:37:50 +0300

    Refactor

--- client/src/components/ui/icons/MaterialIcon.tsx
+++ client/src/components/ui/icons/MaterialIcon.tsx
@@ -2,14 +2,11 @@ import { IMaterialIcon } from '@/components/ui/icons/material-icon.interface'
 import { NextPage } from 'next'
 import * as MaterialIcons from 'react-icons/md'
 
-const MaterialIcon: NextPage<IMaterialIcon> = ({
-	name,
-	fill = 'gray'
-}) => {
+const MaterialIcon: NextPage<IMaterialIcon> = ({ name, fill = 'red' }) => {
 	const IconComponent = MaterialIcons[name]
 
 	return MaterialIcons[name] ? (
-		<IconComponent fill={fill} />
+		<IconComponent fill={fill} className="w-5 h-5" />
 	) : (
 		<MaterialIcons.MdDragIndicator />
 	)
