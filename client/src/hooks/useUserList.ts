ed9fb1977463c13efa91ee118c4f71a76ed95e87
nfdmitry <khv1702@gmail.com>
2024-11-29 22:59:57 +0300

    Added delete user fetch

--- client/src/hooks/useUserList.ts
+++ client/src/hooks/useUserList.ts
@@ -1,16 +1,37 @@
 import { useDebounce } from '@/hooks/useDebounce'
 import UserService from '@/services/user/user.service'
-import { useQuery } from '@tanstack/react-query'
+import {
+	useMutation,
+	useQuery,
+	useQueryClient
+} from '@tanstack/react-query'
+import axios from 'axios'
 import { ChangeEvent, MouseEvent, useState } from 'react'
+import toast from 'react-hot-toast'
 
 const useUserList = () => {
 	const [searchTerm, setSearchTerm] = useState('')
+	const queryClient = useQueryClient()
 	const debouncedSearch = useDebounce(searchTerm, 500)
 
 	const { data, isLoading } = useQuery({
 		queryKey: ['get-user-list', debouncedSearch],
 		queryFn: () => UserService.fetchUserList(debouncedSearch),
 		select: ({ data }) => data
+	})
+
+	const { mutateAsync: deleteAsync } = useMutation({
+		mutationKey: ['delete-user'],
+		mutationFn: (userId: string) => UserService.deleteUser(userId),
+		onSuccess() {
+			toast.success('Delete user was successful')
+			queryClient.invalidateQueries({ queryKey: ['get-user-list'] })
+		},
+		onError(error) {
+			if (axios.isAxiosError(error)) {
+				toast.error(error.response?.data?.message)
+			}
+		}
 	})
 
 	const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
@@ -21,7 +42,14 @@ const useUserList = () => {
 		setSearchTerm('')
 	}
 
-	return { data, isLoading, searchTerm, handleClear, handleSearch }
+	return {
+		data,
+		isLoading,
+		searchTerm,
+		handleClear,
+		handleSearch,
+		deleteAsync
+	}
 }
 
 export default useUserList
