34f4b498e061d7481e432646d29f703d4622c992
nfdmitry <khv1702@gmail.com>
2024-12-05 01:21:51 +0300

    Added component

---
+++ client/src/components/ui/form-elements/admin-page/field-email/FieldEmail.tsx
@@ -0,0 +1,28 @@
+import styles from '@/components/ui/form-elements/admin-page/field-email/FieldEmail.module.scss'
+import { IField } from '@/components/ui/form-elements/form.interface'
+import clsx from 'clsx'
+import { forwardRef } from 'react'
+
+const FieldEmail = forwardRef<HTMLInputElement, IField>(
+	({ error, type = 'text', style, ...rest }, ref) => {
+		return (
+			<div className={clsx(styles['wrapper-input'])} style={style}>
+				<label className={clsx(styles['label-input'])}>
+					<input
+						placeholder="Email"
+						className={clsx(styles['input-field'])}
+						ref={ref}
+						type={type}
+						{...rest}
+						autoComplete="on"
+					/>
+				</label>
+				{error?.message === 'Please enter a valid email' && (
+					<p className={clsx(styles['error-message'])}>{error.message}</p>
+				)}
+			</div>
+		)
+	}
+)
+
+export default FieldEmail
