09e986f09dfd939d816086490b65df2a2813b2ba
nfdmitry <khv1702@gmail.com>
2024-12-05 01:47:15 +0300

    Added component

--- client/src/components/ui/form-elements/admin-page/field-password/FieldPassword.tsx
+++ client/src/components/ui/form-elements/admin-page/field-password/FieldPassword.tsx
@@ -1,14 +1,15 @@
-import styles from '@/components/ui/form-elements/auth-page/field-email/FieldEmail.module.scss'
+import styles from '@/components/ui/form-elements/admin-page/field-password/FieldPassword.module.scss'
 import { IField } from '@/components/ui/form-elements/form.interface'
 import clsx from 'clsx'
 import { forwardRef } from 'react'
 
-const FieldEmail = forwardRef<HTMLInputElement, IField>(
+const FieldPassword = forwardRef<HTMLInputElement, IField>(
 	({ error, type = 'text', style, ...rest }, ref) => {
 		return (
 			<div className={clsx(styles['wrapper-input'])} style={style}>
 				<label className={clsx(styles['label-input'])}>
 					<input
+						placeholder="New password"
 						className={clsx(styles['input-field'])}
 						ref={ref}
 						type={type}
@@ -16,12 +17,13 @@ const FieldEmail = forwardRef<HTMLInputElement, IField>(
 						autoComplete="on"
 					/>
 				</label>
-				{error?.message === 'Please enter a valid email' && (
-					<p className={styles.error}>{error.message.toString()}</p>
+				{error?.message ===
+					'Min length should more 6 symbols. Contains 1 number 0-9, 1 Latin letter a-z, 1 Latin letter A-Z' && (
+					<p className={clsx(styles['error-message'])}>{error.message}</p>
 				)}
 			</div>
 		)
 	}
 )
 
-export default FieldEmail
+export default FieldPassword
