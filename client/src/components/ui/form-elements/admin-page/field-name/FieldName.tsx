1f56d5c31a586bdf4b682abf2c4ebdecb91481e9
nfdmitry <khv1702@gmail.com>
2024-12-08 02:43:42 +0300

    Added component

---
+++ client/src/components/ui/form-elements/admin-page/field-name/FieldName.tsx
@@ -0,0 +1,29 @@
+import styles from '@/components/ui/form-elements/admin-page/field-name/FieldName.module.scss'
+import { IField } from '@/components/ui/form-elements/form.interface'
+import clsx from 'clsx'
+import { forwardRef } from 'react'
+
+const FieldName = forwardRef<HTMLInputElement, IField>(
+	({ error, placeholder, type = 'text', style, ...rest }, ref) => {
+		return (
+			<div className={clsx(styles['wrapper-input'])} style={style}>
+				<label className={clsx(styles['label-input'])}>
+					<input
+						placeholder={placeholder}
+						className={clsx(styles['input-field'])}
+						ref={ref}
+						type={type}
+						{...rest}
+						autoComplete="on"
+					/>
+				</label>
+				{error?.message ===
+					'Min length must be greater than 2 characters. Numbers from the second character and the special character "-" can be used' && (
+					<p className={clsx(styles['error-message'])}>{error.message}</p>
+				)}
+			</div>
+		)
+	}
+)
+
+export default FieldName
---
+++ client/src/components/ui/user-info/UserInfo.tsx
@@ -0,0 +1,45 @@
+import SkeletonLoader from '@/components/ui/skeleton-loader/SkeletonLoader'
+import styles from '@/components/ui/user-info/UserInfo.module.scss'
+import { IUserInfo } from '@/components/ui/user-info/user-info.interface'
+import { NextPage } from 'next'
+import Image from 'next/image'
+
+const UserInfo: NextPage<IUserInfo> = ({
+	avatarPath,
+	name,
+	isLoading
+}: IUserInfo) => (
+	<div className={styles.wrapper}>
+		{isLoading ? (
+			<div className="w-[4.375rem] h-[4.375rem] rounded-full">
+				<SkeletonLoader count={1} className="w-full h-full" />
+			</div>
+		) : avatarPath ? (
+			<Image
+				className={styles.image}
+				src={avatarPath}
+				alt="Avatar"
+				width={70}
+				height={70}
+			/>
+		) : (
+			<Image
+				className={styles.image}
+				src={'/avatar-default.png'}
+				alt="Avatar"
+				width={70}
+				height={70}
+			/>
+		)}
+
+		{isLoading ? (
+			<div className="w-[10rem] h-5">
+				<SkeletonLoader count={1} className="w-full h-full" />
+			</div>
+		) : (
+			<h2 className={styles.subtitle}>{name || 'User'}</h2>
+		)}
+	</div>
+)
+
+export default UserInfo
