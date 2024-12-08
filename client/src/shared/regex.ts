2f97f876b62d218f1bd6126ed4f4fc13e4d83699
nfdmitry <khv1702@gmail.com>
2024-12-08 04:06:08 +0300

    Added valid id regex

--- client/src/shared/regex.ts
+++ client/src/shared/regex.ts
@@ -5,3 +5,5 @@ export const validPassword =
 	/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}/g
 
 export const validName = /^[a-zA-Z][a-zA-Z0-9-]+$/
+
+export const validId = /^[a-z]{2}[a-z0-9]{23}$/
