d966b1145194629a1db3c065e51ebd5dae7864a3
nfdmitry <khv1702@gmail.com>
2024-12-15 23:14:25 +0300

    Fix

--- client/src/shared/types/user.types.ts
+++ client/src/shared/types/user.types.ts
@@ -1,10 +1,10 @@
 import { UserRole } from '@/services/auth/auth.types'
 
 export interface IUser {
-	id: string
+	id: number
 	name?: string
 	email: string
-	password?: string,
+	password?: string
 	avatarPath?: string
 	verificationToken?: string
 	rights: UserRole[]
