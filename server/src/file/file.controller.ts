9c87408214e0e9e20c70613dc3b47b4a04f1a479
nfdmitry <khv1702@gmail.com>
2024-12-19 01:57:54 +0300

    Added controller

---
+++ server/src/file/file.controller.ts
@@ -0,0 +1,27 @@
+import { Auth } from '@/auth/decorators/auth.decorator'
+import { FileService } from '@/file/file.service'
+import {
+	Controller,
+	HttpCode,
+	Post,
+	Query,
+	UploadedFile,
+	UseInterceptors
+} from '@nestjs/common'
+import { FileInterceptor } from '@nestjs/platform-express'
+
+@Controller('files')
+export class FileController {
+	constructor(private readonly fileService: FileService) {}
+
+	@HttpCode(200)
+	@Auth()
+	@Post()
+	@UseInterceptors(FileInterceptor('file'))
+	async uploadFile(
+		@UploadedFile() file: Express.Multer.File,
+		@Query('folder') folder?: string
+	) {
+		return this.fileService.saveFiles([file], folder)
+	}
+}
