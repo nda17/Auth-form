1551624064f766be308d6b959a6e177382c9c864
nfdmitry <khv1702@gmail.com>
2024-12-22 14:48:38 +0300

    Added file service

---
+++ client/src/services/file/file.service.ts
@@ -0,0 +1,20 @@
+import { axiosInterceptorsRequest } from '@/api/interceptors'
+
+class FileService {
+	async upload(file: FormData, folder?: string) {
+		return axiosInterceptorsRequest.post<{ url: string; name: string }[]>(
+			'/files',
+			file,
+			{
+				params: {
+					folder
+				},
+				headers: {
+					'Content-Type': 'multipart/form-data'
+				}
+			}
+		)
+	}
+}
+
+export default new FileService()
