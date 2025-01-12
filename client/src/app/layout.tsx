4e5873466b142689c1b6c44b6aeb66113874e3a2
Dmitry <khv1702@gmail.com>
2025-01-13 02:52:30 +0300

    Fix bugs, editing metadata for seo optimization

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,15 +1,59 @@
 import '@/assets/styles/globals.scss'
 import Layout from '@/components/layout/Layout'
-import MainProvider from '@/providers/Main-provider/MainProvider'
-import type { Metadata } from 'next'
+import CirclesLoader from '@/components/ui/circles-loader/CirclesLoader'
+import MainProvider from '@/providers/main-provider/MainProvider'
+import { Metadata, Viewport } from 'next'
 import { Fira_Mono } from 'next/font/google'
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, Suspense } from 'react'
 
 const inter = Fira_Mono({ subsets: ['latin', 'cyrillic'], weight: '400' })
 
 export const metadata: Metadata = {
-	title: 'Auth-form',
-	description: 'Auth-form'
+	title: {
+		absolute: 'User-auth-template',
+		template: '%s | User-auth-template',
+	},
+	description: 'User-auth-template app.',
+	icons: {
+		icon: '/favicon.svg',
+		shortcut: '/favicon.svg',
+		apple: '/touch-icons/256x256.png',
+		other: {
+			rel: 'touch-icons',
+			url: '/touch-icons/256x256.png',
+			sizes: '256x256',
+			type: 'image/png'
+		}
+	},
+	metadataBase: new URL('http://localgost:3000'),
+	formatDetection: {
+		telephone: false
+	},
+	openGraph: {
+		type: 'website',
+		locale: 'en_US',
+		siteName: 'User-auth-template',
+		images: [
+			{
+				url: '/your-image-for-demo',
+				width: 1200,
+				height: 630,
+				alt: 'User-auth-template app.',
+			},
+		],
+		emails: ['mail@mail.com'],
+	},
+	manifest: '/manifest.json',
+	publisher: 'User-auth-template',
+	authors: {
+		name: 'User-auth-template',
+		url: 'http://localhost:3000'
+	},
+	applicationName: 'User-auth-template app.'
+}
+
+export const viewport: Viewport = {
+	themeColor: '#ffffff',
 }
 
 const RootLayout = ({ children }: PropsWithChildren<unknown>) => {
@@ -17,7 +61,11 @@ const RootLayout = ({ children }: PropsWithChildren<unknown>) => {
 		<html lang="en">
 			<body className={inter.className}>
 				<MainProvider>
-					<Layout>{children}</Layout>
+					<Layout>
+						<Suspense fallback={<CirclesLoader />}>
+							{children}
+						</Suspense>
+					</Layout>
 				</MainProvider>
 			</body>
 		</html>
