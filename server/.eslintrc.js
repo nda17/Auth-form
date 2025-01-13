54b8d049eb34bba219cb2dfe134379f09ffceade
Dmitry <khv1702@gmail.com>
2025-01-14 00:50:44 +0300

    Editing config

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -3,25 +3,23 @@ module.exports = {
 	parserOptions: {
 		project: 'tsconfig.json',
 		tsconfigRootDir: __dirname,
-		sourceType: 'module',
+		sourceType: 'module'
 	},
 	plugins: ['@typescript-eslint/eslint-plugin'],
 	extends: [
 		'plugin:@typescript-eslint/recommended',
-		'plugin:prettier/recommended',
+		'plugin:prettier/recommended'
 	],
 	root: true,
 	env: {
 		node: true,
-		jest: true,
+		jest: true
 	},
 	ignorePatterns: ['.eslintrc.js'],
 	rules: {
 		'@typescript-eslint/interface-name-prefix': 'off',
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
-		'no-console': 1,
-		'prettier/prettier': 0,
-	},
+		'@typescript-eslint/no-explicit-any': 'off'
+	}
 }
