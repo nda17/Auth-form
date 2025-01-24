3a2c6012b437105557be77ba88409d835ba40190
Dmitry <khv1702@gmail.com>
2025-01-25 01:22:08 +0300

    Added component

---
+++ client/src/components/ui/form-elements/universal-elements/show-more-button/ShowMoreButton.tsx
@@ -0,0 +1,20 @@
+import { IShowMoreButton } from '@/components/ui/form-elements/universal-elements/show-more-button/show-more-button.interface';
+import styles from '@/components/ui/form-elements/universal-elements/show-more-button/ShowMoreButton.module.scss';
+import { NextPage } from 'next';
+
+const ShowMoreButton: NextPage<IShowMoreButton> = ({
+	isLoading,
+	onLoadMore
+}) => {
+	return (
+		<button
+			onClick={onLoadMore}
+			disabled={isLoading}
+			className={styles.button}
+		>
+			{isLoading ? 'Wait..' : 'Show more'}
+		</button>
+	);
+};
+
+export default ShowMoreButton;
