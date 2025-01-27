66e04c4c412fca46936d392179a811f97e9e90e2
Dmitry <khv1702@gmail.com>
2025-01-28 01:08:16 +0300

    Added component

---
+++ client/src/components/screens/admin/users/ManageUsersShowMore.tsx
@@ -0,0 +1,21 @@
+import ShowMoreButton from '@/components/ui/form-elements/universal-elements/show-more-button/ShowMoreButton';
+import { Dispatch, SetStateAction } from 'react';
+
+interface IManageUsersShowMore {
+	loading: boolean;
+	setPage: Dispatch<SetStateAction<number>>;
+}
+
+const ManageUsersShowMore = ({
+	setPage,
+	loading
+}: IManageUsersShowMore) => {
+	return (
+		<ShowMoreButton
+			isLoading={loading}
+			onLoadMore={() => setPage(prev => prev + 1)}
+		/>
+	);
+};
+
+export default ManageUsersShowMore;
