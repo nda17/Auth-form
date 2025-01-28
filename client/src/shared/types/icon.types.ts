1f8797cb874de549ccc44adb97aeddbbade371e2
Dmitry <khv1702@gmail.com>
2025-01-28 04:50:59 +0300

    Added types

--- client/src/shared/types/icon.types.ts
+++ client/src/shared/types/icon.types.ts
@@ -1,5 +1,7 @@
+import * as icons from 'lucide-react';
 import * as FontAwesomeIcons from 'react-icons/fa';
 import * as MaterialIcons from 'react-icons/md';
 
 export type TypeMaterialIconName = keyof typeof MaterialIcons;
 export type TypeFontAwesomeName = keyof typeof FontAwesomeIcons;
+export type TypeLucideIconName = keyof typeof icons;
