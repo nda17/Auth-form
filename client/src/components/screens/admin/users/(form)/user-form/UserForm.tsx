92c6c5800f4265b59d6f5b8788029480cae907a8
Dmitry <khv1702@gmail.com>
2025-01-29 01:58:51 +0300

    Added component

---
+++ client/src/components/screens/admin/users/(form)/user-form/UserForm.tsx
@@ -0,0 +1,28 @@
+'use client';
+import UserEditingForm from '@/components/screens/admin/users/(form)/user-editing-form/UserEditingForm';
+import { useCreateUpdateUser } from '@/components/screens/admin/users/(form)/user-form/useCreateUpdateUser';
+import { IUserForm } from '@/components/screens/admin/users/(form)/user-form/user-form.types';
+import { NextPage } from 'next';
+
+const UserForm: NextPage<IUserForm> = ({ type, params }) => {
+	const id = params.id;
+	const result = useCreateUpdateUser(
+		id,
+		type === 'admin-create',
+		type === 'admin-edit',
+		type === 'user-edit-profile'
+	);
+
+	return (
+		<UserEditingForm
+			queriesResult={result}
+			id={id}
+			isCreateForm={type === 'admin-create'}
+			isAdminEdit={type === 'admin-edit'}
+			isUserEdit={type === 'user-edit-profile'}
+			type={type}
+		/>
+	);
+};
+
+export default UserForm;
