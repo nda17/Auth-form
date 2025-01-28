d385fb1320151bcae7eaec2ec2b695d208b13bb7
Dmitry <khv1702@gmail.com>
2025-01-28 17:38:35 +0300

    Added interface

---
+++ client/src/components/screens/admin/users/(form)/fields/fields.interface.ts
@@ -0,0 +1,23 @@
+import { CSSProperties, InputHTMLAttributes } from 'react';
+import { FieldError, FieldErrorsImpl, Merge } from 'react-hook-form';
+
+export interface IFieldProps {
+	error?: FieldError | Merge<FieldError, FieldErrorsImpl<any>> | undefined;
+}
+
+type TypeInputPropsField = InputHTMLAttributes<HTMLInputElement> &
+	IFieldProps;
+
+export interface IField extends TypeInputPropsField {}
+
+export interface IUploadField {
+	currentFile?: string;
+	folder?: string;
+	value?: string;
+	onChange: (...event: any[]) => void;
+	placeholder: string;
+	error?: FieldError;
+	style?: CSSProperties;
+}
+
+// export interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {}
