7207a1e4d5ecaea7f3a81bef4cb17e881127565f
Dmitry <khv1702@gmail.com>
2025-01-28 18:11:25 +0300

    Fix

--- client/src/components/ui/sub-heading/SubHeading.tsx
+++ client/src/components/ui/sub-heading/SubHeading.tsx
@@ -1,9 +1,10 @@
 import styles from '@/components/ui/sub-heading/SubHeading.module.scss';
 import { ISubHeading } from '@/components/ui/sub-heading/sub-heading.interface';
+import clsx from 'clsx';
 import { NextPage } from 'next';
 
 const SubHeading: NextPage<ISubHeading> = ({ text }) => {
-	return <h2 className={styles.subHeading}>{text}</h2>;
+	return <h2 className={clsx(styles['sub-heading'])}>{text}</h2>;
 };
 
 export default SubHeading;
