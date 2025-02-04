2f83aa4c4d89bf2b9690e24a417ad42c592ed1ff
Dmitry <khv1702@gmail.com>
2025-02-04 20:10:05 +0300

    Added args

--- client/src/services/file/file.service.ts
+++ client/src/services/file/file.service.ts
@@ -1,13 +1,14 @@
 import { axiosInterceptorsRequest } from '@/api/interceptors';
 
 class FileService {
-	async upload(file: FormData, folder?: string) {
+	async upload(file: FormData, folder?: string, id?: string) {
 		return axiosInterceptorsRequest.post<{ url: string; name: string }[]>(
 			'/files',
 			file,
 			{
 				params: {
-					folder
+					folder,
+					id
 				},
 				headers: {
 					'Content-Type': 'multipart/form-data'
