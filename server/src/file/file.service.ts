53029a9568aaa2e644dc3ab2c51b043b95c810ca
Dmitry <khv1702@gmail.com>
2025-02-04 19:03:58 +0300

    Added functionality for replacing the name of the uploaded user avatar file with its identifier

--- server/src/file/file.service.ts
+++ server/src/file/file.service.ts
@@ -2,26 +2,30 @@ import { IFileResponse } from '@/file/file.interface';
 import { Injectable } from '@nestjs/common';
 import { path } from 'app-root-path';
 import { ensureDir, writeFile } from 'fs-extra';
+import { extname } from 'path';
 
 @Injectable()
 export class FileService {
 	async saveFiles(
 		files: Express.Multer.File[],
-		folder: string = 'default'
+		folder: string = 'default',
+		id: string
 	): Promise<IFileResponse[]> {
 		const uploadFolder = `${path}/uploads/${folder}`;
+
 		await ensureDir(uploadFolder);
 
 		const res: IFileResponse[] = await Promise.all(
 			files.map(async file => {
-				await writeFile(
-					`${uploadFolder}/${file.originalname}`,
-					file.buffer
-				);
+				const originalFileName = file.originalname;
+				const extension = extname(originalFileName);
+				const newFileName = `${id}${extension}`;
+
+				await writeFile(`${uploadFolder}/${newFileName}`, file.buffer);
 
 				return {
-					url: `/uploads/${folder}/${file.originalname}`,
-					name: file.originalname
+					url: `/uploads/${folder}/${newFileName}`,
+					name: newFileName
 				};
 			})
 		);
