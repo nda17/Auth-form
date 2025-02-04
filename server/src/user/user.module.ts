c5d8ed446f0436b5349f7f64ee36fd715c766aba
Dmitry <khv1702@gmail.com>
2025-02-04 19:46:38 +0300

    Import FileService module

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,3 +1,4 @@
+import { FileService } from '@/file/file.service';
 import { PrismaService } from '@/prisma.service';
 import { UserController } from '@/user/user.controller';
 import { UserService } from '@/user/user.service';
@@ -5,7 +6,7 @@ import { Module } from '@nestjs/common';
 
 @Module({
 	controllers: [UserController],
-	providers: [UserService, PrismaService],
+	providers: [UserService, FileService, PrismaService],
 	exports: [UserService]
 })
 export class UserModule {}
