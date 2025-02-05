934040f4d2fd80aea2e00775723e5a9c88dbfdaf
Dmitry <khv1702@gmail.com>
2025-02-06 02:33:28 +0300

    Refactor

--- client/src/components/screens/profile/Profile.tsx
+++ client/src/components/screens/profile/Profile.tsx
@@ -1,19 +1,65 @@
 'use client';
 import styles from '@/components/screens/profile/Profile.module.scss';
-import CirclesLoader from '@/components/ui/circles-loader/CirclesLoader';
+import { useUser } from '@/components/screens/profile/useUser';
 import Heading from '@/components/ui/heading/Heading';
-import UserInfo from '@/components/ui/user-info/UserInfo';
+import SkeletonLoader from '@/components/ui/skeleton-loader/SkeletonLoader';
 import { PUBLIC_PAGES } from '@/config/pages/public.config';
-import useUser from '@/hooks/useUser';
 import authService from '@/services/auth/auth.service';
+import { formatDate } from '@/utils/format.date';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { clsx } from 'clsx';
-import { NextPage } from 'next';
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { useTransition } from 'react';
+import { FC, useTransition } from 'react';
 import toast from 'react-hot-toast';
+const DynamicUserInfo = dynamic(
+	() => import('@/components/ui/user-info/UserInfo'),
+	{
+		loading: () => (
+			<div className="mb-[1.1rem] gap-4 flex items-center">
+				<div className="w-[5rem] h-[5rem] rounded-full">
+					<SkeletonLoader count={1} circle className="w-full h-full" />
+				</div>
+				<div className="w-[10rem] h-5">
+					<SkeletonLoader count={1} className="w-full h-full" />
+				</div>
+			</div>
+		),
+		ssr: false
+	}
+);
+const DynamicConfirmStatus = dynamic(
+	() => import('@/components/ui/profile/confirm-status/ConfirmStatus'),
+	{
+		loading: () => (
+			<SkeletonLoader count={1} className="w-full h-7 mb-5" />
+		),
+		ssr: false
+	}
+);
+const DynamicProfileInfoDate = dynamic(
+	() =>
+		import('@/components/ui/profile/profile-info-field/ProfileInfoField'),
+	{
+		loading: () => (
+			<SkeletonLoader count={1} className="w-full h-7 mb-5" />
+		),
+		ssr: false
+	}
+);
+const DynamicProfileInfoEmail = dynamic(
+	() =>
+		import('@/components/ui/profile/profile-info-field/ProfileInfoField'),
+	{
+		loading: () => (
+			<SkeletonLoader count={1} className="w-full h-7 mb-[2.4rem]" />
+		),
+		ssr: false
+	}
+);
 
-const Profile: NextPage = () => {
+const Profile: FC = () => {
 	const { user, isLoading } = useUser();
 	const { replace } = useRouter();
 	const queryClient = useQueryClient();
@@ -39,36 +85,35 @@ const Profile: NextPage = () => {
 	return (
 		<div className={styles.wrapper}>
 			<Heading text="Profile" />
+			<div className={clsx(styles['user-info-wrapper'])}>
+				<DynamicUserInfo
+					avatarPath={user?.avatarPath || null}
+					name={user?.name || null}
+					isLoading={isLoading}
+				/>
+				<Link
+					href={`${PUBLIC_PAGES.USER_PROFILE_EDIT}/${user.id}`}
+					className={clsx(styles['edit-link'])}
+					aria-label="Edit profile"
+				>
+					Edit
+				</Link>
+			</div>
+			<DynamicConfirmStatus verificationToken={user?.verificationToken} />
+			<DynamicProfileInfoDate
+				title="Date register:"
+				content={formatDate(user?.createdAt)}
+			/>
+			<DynamicProfileInfoEmail title="Email:" content={user?.email} />
 
-			{isLoading ? (
-				<CirclesLoader />
-			) : (
-				<>
-					<UserInfo
-						avatarPath={user?.avatarPath}
-						name={user?.name}
-						isLoading={isLoading}
-					/>
-					<p className={clsx(styles['info-field'])}>
-						Email: {user.email}{' '}
-						<i>
-							(
-							{user.verificationToken
-								? 'Confirmation is required'
-								: 'Confirmed'}
-							)
-						</i>
-					</p>
-					<button
-						aria-label="Logout"
-						onClick={() => mutateLogout()}
-						disabled={isLogoutLoading}
-						className={clsx(styles['logout-button'])}
-					>
-						{isLogoutLoading ? 'Wait...' : 'Logout'}
-					</button>
-				</>
-			)}
+			<button
+				aria-label="Logout"
+				onClick={() => mutateLogout()}
+				disabled={isLogoutLoading}
+				className={clsx(styles['logout-button'])}
+			>
+				{isLogoutLoading ? 'Wait...' : 'Logout'}
+			</button>
 		</div>
 	);
 };
