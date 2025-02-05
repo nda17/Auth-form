8eec774d9d69784f68624c12db0b82b6e4af252e
Dmitry <khv1702@gmail.com>
2025-02-05 10:48:48 +0300

    Added util time-stamp

---
+++ server/src/utils/time-stamp.util.ts
@@ -0,0 +1,11 @@
+export const timeStamp = () => {
+	const date = new Date();
+	const year = date.getFullYear();
+	const month = `0${date.getMonth() + 1}`.slice(-2);
+	const day = `0${date.getDate()}`.slice(-2);
+	const hour = `0${date.getHours()}`.slice(-2);
+	const minute = `0${date.getMinutes()}`.slice(-2);
+	const second = `0${date.getSeconds()}`.slice(-2);
+
+	return `${year}-${month}-${day}_${hour}-${minute}-${second}`;
+};
