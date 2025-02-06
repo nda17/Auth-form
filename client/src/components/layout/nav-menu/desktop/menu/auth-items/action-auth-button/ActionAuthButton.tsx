a05558f661faa874b8c64666fb16bce39d945e76
Dmitry <khv1702@gmail.com>
2025-02-06 04:46:29 +0300

    Refactor

--- client/src/components/layout/nav-menu/desktop/menu/auth-items/action-auth-button/ActionAuthButton.tsx
+++ client/src/components/layout/nav-menu/desktop/menu/auth-items/action-auth-button/ActionAuthButton.tsx
@@ -1,11 +1,19 @@
 import { IActionAuthButton } from '@/components/layout/nav-menu/desktop/menu/auth-items/action-auth-button/action-auth-button.interface';
 import LogoutButton from '@/components/layout/nav-menu/desktop/menu/logout-button/LogoutButton';
 import MenuItem from '@/components/layout/nav-menu/desktop/menu/menu-item/MenuItem';
+import SkeletonLoader from '@/components/ui/skeleton-loader/SkeletonLoader';
 import { PUBLIC_PAGES } from '@/config/pages/public.config';
 import { FC } from 'react';
 
-const ActionAuthButton: FC<IActionAuthButton> = ({ isLoggedIn }) => {
-	return isLoggedIn ? (
+const ActionAuthButton: FC<IActionAuthButton> = ({
+	isLoading,
+	isLoggedIn
+}) => {
+	return isLoading ? (
+		<div className="w-[4.903rem] h-full flex flex-col justify-center">
+			<SkeletonLoader count={1} className="w-full h-4" />
+		</div>
+	) : isLoggedIn ? (
 		<LogoutButton />
 	) : (
 		<MenuItem
